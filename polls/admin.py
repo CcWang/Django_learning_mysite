f620a8ad6b2802ce0b9fe406761f2edf7326b270
CC Wang <chongchong1110@gmail.com>
2016-06-13 22:24:26 -0700

    add more urls

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# to link models to admin.py
+from .models import Question
+from .models import Choice
+# link Question model to admin
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -6,5 +6,12 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse('Hellp, World. You are at the polls index')
 
-def test(request):
-  return HttpResponse('Hellp, again test')
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request,question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
